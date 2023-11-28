28c564cc9686b2be6e34b269daa8a4a169da9b9d
thermvs <mariagewitter@gmail.com>
2023-11-28 18:59:14 +0300

    feat: add HsBoxOfficeApplicationTests

---
+++ src/test/java/artgallery/hsboxoffice/HsBoxOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package artgallery.hsboxoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsBoxOfficeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
