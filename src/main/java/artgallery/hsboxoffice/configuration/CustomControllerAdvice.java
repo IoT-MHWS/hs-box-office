4b996d368283b1e496cbfcddeea156cedb5c1ab9
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-12-17 12:38:10 +0300

    fix: error handling

--- src/main/java/artgallery/hsboxoffice/configuration/CustomControllerAdvice.java
+++ src/main/java/artgallery/hsboxoffice/configuration/CustomControllerAdvice.java
@@ -4,34 +4,36 @@ import artgallery.hsboxoffice.controller.ApiError;
 import artgallery.hsboxoffice.exception.DatabaseConflictException;
 import artgallery.hsboxoffice.exception.DoesNotExistException;
 import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @ControllerAdvice
-public class CustomControllerAdvice {
+public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler({
             DoesNotExistException.class
     })
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError notFound(Exception ex) {
-
-        return new ApiError(HttpStatus.NOT_FOUND, ex);
+    public ResponseEntity<?> notFound(Exception ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body(new ApiError(HttpStatus.NOT_FOUND, ex.getMessage()));
     }
 
     @ResponseBody
     @ExceptionHandler({
             DatabaseConflictException.class
     })
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError exIndb(Exception ex) {
-
-        return new ApiError(HttpStatus.CONFLICT, ex);
+    public ResponseEntity<?> exIndb(Exception ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+            .body(new ApiError(HttpStatus.CONFLICT, ex.getMessage()));
     }
 
     @ResponseBody
@@ -39,18 +41,19 @@ public class CustomControllerAdvice {
             IllegalArgumentException.class,
             MethodArgumentNotValidException.class
     })
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public ApiError notCorrect(Exception ex) {
+    public ResponseEntity<?> notCorrect(Exception ex) {
 
-        return new ApiError(HttpStatus.UNPROCESSABLE_ENTITY, ex);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY)
+            .body(new ApiError(HttpStatus.UNPROCESSABLE_ENTITY, ex.getMessage()));
     }
 
     @ResponseBody
     @ExceptionHandler({
         FeignException.class
     })
-    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
-    public ApiError feignException(Exception ex) {
-        return new ApiError(HttpStatus.UNPROCESSABLE_ENTITY, ex);
+    public ResponseEntity<?> feignException(FeignException ex) {
+        log.info(ex.getMessage());
+        return ResponseEntity.status(ex.status())
+            .body(ex.contentUTF8());
     }
 }
