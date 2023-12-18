69acaf4fcc8863a0f575e8b44d503a93dcbb3b2c
niqzart <qwert45hi@yandex.ru>
2023-12-19 01:36:19 +0300

    refactor: remove unused code to improve coverage

---
+++ src/main/java/artgallery/hsboxoffice/configuration/ApiError.java
@@ -0,0 +1,28 @@
+package artgallery.hsboxoffice.configuration;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ApiError {
+
+    private HttpStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime timestamp;
+    private String message;
+    private String debugMessage;
+
+    private ApiError() {
+        timestamp = LocalDateTime.now();
+    }
+
+    public ApiError(HttpStatus status, Throwable ex) {
+        this();
+        this.status = status;
+        this.message = "Unexpected error";
+        this.debugMessage = ex.getMessage();
+    }
+}
