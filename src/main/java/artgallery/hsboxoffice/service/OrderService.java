65eb14994a64e4ebc73b317aa89a8447e2fa5f95
Nikita Kulakov <nvkulakov.dev@gmail.com>
2024-01-15 16:21:16 +0300

    refactor: to match clean architecture

--- src/main/java/artgallery/hsboxoffice/service/OrderService.java
+++ src/main/java/artgallery/hsboxoffice/service/OrderService.java
@@ -2,62 +2,18 @@ package artgallery.hsboxoffice.service;
 
 import artgallery.hsboxoffice.dto.OrderCreateDTO;
 import artgallery.hsboxoffice.dto.OrderDTO;
-import artgallery.hsboxoffice.exception.OrderDoesNotExistException;
-import artgallery.hsboxoffice.model.OrderEntity;
-import artgallery.hsboxoffice.repository.OrderRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@Service
-@RequiredArgsConstructor
-public class OrderService {
-    private final OrderRepository orderRepository;
+public interface OrderService {
+  Flux<OrderDTO> getAllOrders(Pageable pageable);
 
-    public Flux<OrderDTO> getAllOrders(Pageable pageable) {
-        return orderRepository.findAllBy(pageable)
-                .map(this::mapToOrderDto);
-    }
+  Mono<OrderDTO> getOrderById(Long id);
 
-    public Mono<OrderDTO> getOrderById(Long id) {
-        return orderRepository.findById(id)
-                .switchIfEmpty(Mono.error(new OrderDoesNotExistException(id)))
-                .map(this::mapToOrderDto);
-    }
+  Mono<OrderDTO> createOrder(OrderCreateDTO orderCreateDTO);
 
-    public Mono<OrderDTO> createOrder(OrderCreateDTO orderCreateDTO) {
-        OrderEntity orderEntity = this.mapToOrderEntity(orderCreateDTO);
-        return orderRepository.save(orderEntity)
-                .map(this::mapToOrderDto);
-    }
+  Mono<OrderDTO> updateOrder(long id, OrderCreateDTO orderCreateDTO);
 
-    public Mono<OrderDTO> updateOrder(long id, OrderCreateDTO orderCreateDTO) {
-        return orderRepository.findById(id)
-                .switchIfEmpty(Mono.error(new OrderDoesNotExistException(id)))
-                .flatMap(existingOrder -> {
-                    existingOrder.setDate(orderCreateDTO.getDate());
-                    existingOrder.setLogin(orderCreateDTO.getLogin());
-                    return orderRepository.save(existingOrder);
-                })
-                .map(this::mapToOrderDto);
-    }
-
-    public Mono<Void> deleteOrder(long id) {
-        return orderRepository.deleteById(id);
-    }
-
-    private OrderEntity mapToOrderEntity(OrderCreateDTO orderCreateDTO) {
-        OrderEntity orderEntity = new OrderEntity();
-        orderEntity.setDate(orderCreateDTO.getDate());
-        orderEntity.setLogin(orderCreateDTO.getLogin());
-        return orderEntity;
-    }
-
-    private OrderDTO mapToOrderDto(OrderEntity orderEntity) {
-        return new OrderDTO(orderEntity.getId(),
-                orderEntity.getDate(), orderEntity.getLogin());
-    }
+  Mono<Void> deleteOrder(long id);
 }
-
